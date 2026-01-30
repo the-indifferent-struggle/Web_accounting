b4f992523fa07d4582c1bb12f7933a6d2722d1b1
yxwf <123456789@yxwf.cn>
2026-01-30 22:31:10 +0800

    第一次提交

--- app.py
+++ app.py
@@ -2,5 +2,15 @@ from flask import Flask,render_template,request
 from config import Wa_config
 import pymysql
 
-conn = pymysql.connect(**Wa_config)
+conn = pymysql.connect(**Wa_config) #字典解包
 cursor = conn.cursor(cursor = pymysql.cursor.DictCursor)
+
+app = Flask(__name__)
+
+@app.route("/user/login",methods = ["GET","POST"])
+def login():
+    return render_template("login.html")
+
+
+if __name__ == "__main__":
+    app.run()
