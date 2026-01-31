bfa20be277168e1c2e6bd737b9c9ca076cd75df9
yxwf <123456789@yxwf.cn>
2026-01-31 22:10:54 +0800

    第二次提交

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ from flask import Flask,render_template,request
 from config import Wa_config
 import pymysql
 
-conn = pymysql.connect(**Wa_config) #字典解包
-cursor = conn.cursor(cursor = pymysql.cursor.DictCursor)
+
 
 app = Flask(__name__)
 
@@ -11,6 +10,9 @@ app = Flask(__name__)
 def login():
     return render_template("login.html")
 
+@app.route("/user/register",methods = ["GET","POST"])
+def register():
+    return render_template("register.html")
 
 if __name__ == "__main__":
     app.run()
